9299cf6e3ae8736d8b53d595312ab6d0b49bd8fc
kevapostol <apostolkevin@gmail.com>
2019-10-27 09:34:18 -0700

    all

--- main.c
+++ main.c
@@ -31,12 +31,7 @@ void _printf(char *format, ...)
  */
 int main(void)
 {
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
-
 	_printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
+	printf("Let's try to printf a simple sentence.\n");
 	return (0);
 }
