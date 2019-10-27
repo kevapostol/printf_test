6c043e8306cb961b476aeeae13ef0e770017bec8
kevapostol <apostolkevin@gmail.com>
2019-10-27 09:02:15 -0700

    test

--- main.c
+++ main.c
@@ -19,14 +19,14 @@ int main(void)
 	return (0);
 }
 
-void _printf(char* format, ...)
+void _printf(char *format, ...)
 {
 	char *traverse;
 	unsigned int i;
 	char *s;
 
 	va_list arg;
-	
+
 	va_start(arg, format);
 
 	for (traverse = format; *traverse != '\0'; traverse++)
